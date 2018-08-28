f592c0380bd2644d05778ce1cdf63e7994c06dbd
Moritz Angermann <moritz.angermann@gmail.com>
2018-08-28 12:10:43 +0800

    :(

--- default.nix
+++ default.nix
@@ -183,7 +183,7 @@ let
       repo = "stack2nix";
       rev = "c51db2d31892f7c4e7ff6acebe4504f788c56dca";
       sha256 = "10jcj33sxpq18gxf3zcck5i09b2y4jm6qjggqdlwd9ss86wg3ksb";
-    }) { inherit pkgs; cabal2nix = cabal2nix; };
+    }) { inherit (cardanoPkgs // other); };
     inherit (pkgs) purescript;
     connectScripts = {
       mainnet = {
